a7bd6db378ef3d6c40a0052612c53ba6a94d9908
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-10-27 21:15:24 +0530

    feat: redis wiring, attemp view

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Liveblocks = require("@liveblocks/node").Liveblocks;
 const cors = require("cors");
+const redis = require("redis");
 const crypto = require("crypto");
 
 require("dotenv").config();
@@ -15,34 +16,47 @@ app.use(
   })
 );
 app.use(express.json());
+const client = redis.createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
+client.on("error", (err) => console.log("Redis Client Error", err));
+client.on("connect", () => console.log("Redis Client Connected"));
+
 const liveblocks = new Liveblocks({ secret: process.env.LB_SECRET_KEY });
 
 async function startServer() {
   try {
+    await client.connect();
     app.post("/api/create-room", async (req, res) => {
       try {
         const roomID = crypto.randomUUID();
-        const quizID = 100000 + Math.floor(Math.random() * 909999);
-        console.log({ question: req.body.questions });
+        const quizID = (100000 + Math.floor(Math.random() * 909999)).toString();
+        const { questions } = req.body;
+        await client.set(quizID, JSON.stringify(questions));
+
         const room = await liveblocks.createRoom(roomID, {
           // The default room permissions. `[]` for private, `["room:write"]` for public.
           defaultAccesses: ["room:write"],
           // Optional, custom metadata to attach to the room
           metadata: {
-            quizID: JSON.stringify(quizID),
-            questions: JSON.stringify(req.body.questions),
+            quizID,
           },
         });
-        res.status(200).json({ id: room.id, metadata: room.metadata });
+        res.status(200).json({ id: room.id, questions, quizID });
       } catch (err) {
         console.error("Failed", err);
         res.status(500).json({ error: "Could not launch the quiz" });
       }
     });
-    // Get room
+    // ID is the quiz ID
     app.get("/api/get-room/:id", async (req, res) => {
       try {
         const { id } = req.params;
+        const reply = await client.get(id);
         const { data: rooms } = await liveblocks.getRooms({
           limit: 1,
           query: {
@@ -52,8 +66,8 @@ async function startServer() {
             },
           },
         });
-        console.log(rooms);
-        res.status(200).json({ id: rooms[0].id, metadata: rooms[0].metadata });
+        console.log(reply);
+        res.status(200).json({ id: rooms[0].id, questions: reply });
       } catch (err) {
         console.error(`Error fetching quiz:`, err);
         res.status(500).json({ error: "Could not find the quiz" });
